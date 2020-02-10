6824380616742cee9c063e0d3a32887e3167f759
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2020-02-11 00:31:36 +0100

    build(scripts): automate version release (#24)

---
+++ scripts/ci/release.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const path = require("path");
+const shelljs = require("shelljs");
+require("colors");
+
+const { version: VERSION } = require("../../package");
+const README_PATH = path.join(__dirname, "../../README.md");
+
+/**
+ * Run a command in the shell.
+ *
+ * @param {string} command
+ */
+function run(command) {
+  shelljs.echo(`Running: \`${command}\`…`.blue);
+  const output = shelljs.exec(command);
+  if (output.code !== 0) shelljs.exit(1);
+}
+
+/* eslint-disable */
+const NEW_README_LINKS = `
+<!-- CI_START -->
+[link-heroku]: https://heroku.com/deploy?template=https://github.com/SocialGouv/monitorer/tree/v${VERSION}
+<!-- CI_END -->
+`.trim();
+/* eslint-enable */
+
+(() => {
+  try {
+    run(`git checkout -B v${VERSION}`);
+    const readmeSource = fs.readFileSync(README_PATH, "utf8");
+    const newReadmeSource = readmeSource.replace(/<!-- CI_START.*CI_END -->/s, NEW_README_LINKS);
+    fs.writeFileSync(README_PATH, newReadmeSource);
+    run(`git add .`);
+  } catch (err) {
+    shelljs.echo(`[scripts/ci/release.js] Error: ${err.message}`.red);
+
+    shelljs.exit(1);
+  }
+})();
