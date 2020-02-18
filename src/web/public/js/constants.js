476c29459afd3356e57e538ac614c72f1e88b8fa
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2020-02-18 22:49:34 +0100

    feat(web): add duration hash (#50)

--- src/web/public/js/constants.js
+++ src/web/public/js/constants.js
@@ -1,16 +1,10 @@
 export const DURATION = {
   ONE_DAY: "ONE_DAY",
-  ONE_HOUR: "ONE_DAY",
-  ONE_WEEK: "ONE_DAY",
+  ONE_HOUR: "ONE_HOUR",
+  ONE_WEEK: "ONE_WEEK",
 };
 
 export const EVENT = {
   UPDATE_CHECKPOINTS: "UPDATE_CHECKPOINTS",
   UPDATE_DURATION: "UPDATE_DURATION",
-};
-
-export const SERVICE_TYPE = {
-  HTML: "html",
-  JSON: "json",
-  PING: "ping",
 };
